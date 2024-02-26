c2e4ac89c060e04f7b01e3b46ea5aae4485a60cf
Kolt_Horror <kolt.horror@gmail.com>
2024-02-27 10:39:48 +1100

    fixed base_url var setting

--- root/src/main/python/deallocate_ip/source.py
+++ root/src/main/python/deallocate_ip/source.py
@@ -103,7 +103,7 @@ def do_api_key_check(base_url, auth_credentials, cert):
 # Function that orchestrates the deallocation of IPs.
 def do_deallocate_ip(self, auth_credentials, cert):
     # Construct the base URL for the IPAM API call.
-    base_url = f"https://{self.inputs['endpointProperties']['hostName']}/api/{auth_credentials['privateKeyId']}"
+    base_url = f"https://{self.inputs['endpoint']['endpointProperties']['hostName']}/api/{auth_credentials['privateKeyId']}"
 
     # Validate the API key and return the headers for use in subsequent API calls.
     headers = do_api_key_check(base_url, auth_credentials, cert)
